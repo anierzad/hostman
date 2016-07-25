15c8522b21405f38085e7dd08a8e24796804e884
Adam Nierzad <a.nierzad@gmail.com>
2016-07-25 21:24:28 +0100

    Fixed issue in prompts which was stopping application closing.

--- prompts/index.js
+++ prompts/index.js
@@ -2,14 +2,14 @@
 // This module will provide prompts to get information from the user.
 var readline = require('readline');
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 module.exports = {
 
     hostsFilePath: function (callback) {
+
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
         rl.question('Where is your hosts file located?\n', function (response) {
             rl.close();
@@ -19,6 +19,11 @@ module.exports = {
     },
 
     devMachineAddress: function (callback) {
+
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
         rl.question('What is the IP address of your development machine?\n', function (response) {
             rl.close();
