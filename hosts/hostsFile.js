22819a92ae21a70bb827b82efd36a96dbecfa58d
Adam Nierzad <a.nierzad@gmail.com>
2016-07-25 21:25:04 +0100

    Extending hosts file.

--- hosts/hostsFile.js
+++ hosts/hostsFile.js
@@ -2,21 +2,61 @@
 // Manages manipulation of the hosts file.
 
 var fs = require('fs');
+var readline = require('readline');
 
 var config = require('../config');
 
 module.exports = {
 
+    // Add a host to the hosts file.
     addHost: function (host, callback) {
-        
+
+        // Get the hosts file path.
         config.hostsFilePath(function (err, path) {
 
+            // Is there an error?
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            // Get the dev machine address.
             config.devMachineAddress(function (err, address) {
 
+                // Is there an error?
+                if (err) {
+                    callback(err);
+                    return;
+                }
+
+                // Open file.
                 fs.open(path, 'r+', function (err, fd) {
+
+                    // Is there an error?
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
 
                     fs.write(fd, '\n' + address + '\t' + host + ' // hostman managed');
                 });
+            });
+        });
+    },
+
+    // Remove a host from the hosts file.
+    removeHost: function (host, callback) {
+
+        // Get the hosts file path.
+        config.hostsFilePath(function (err, path) {
+
+            // Create line reader.
+            var lineReader = readline.createInterface({
+                input: fs.createReadStream(path)
+            });
+
+            rl.on('line', function (line) {
+                console.log(line);
             });
         });
     }
